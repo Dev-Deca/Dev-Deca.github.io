e3a685522a766aa41bdf872e393682b0eb2c0762
De-Caffeinated <151108135+Dev-Deca@users.noreply.github.com>
2023-11-26 21:34:29 +0900

    modified page

--- script.js
+++ script.js
@@ -1,47 +1,3 @@
-document.addEventListener('mousemove', function(e) {
-    const x = e.clientX;
-    const y = e.clientY;
-
-    document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, img').forEach(function(el) {
-        if (el.classList.contains('email-info')) {
-            return;
-        }
-        
-        const rect = el.getBoundingClientRect();
-        const elX = rect.left + rect.width / 2;
-        const elY = rect.top + rect.height / 2;
-        const distance = Math.sqrt((elX - x) ** 2 + (elY - y) ** 2);
-        const maxDistance = 500; // 최대 효과 거리
-        const brightness = Math.max(100 - (distance / maxDistance * 100), 50); // 최대 100, 최소 50
-
-        if (el.tagName === 'IMG') {
-            el.style.filter = `brightness(${brightness}%)`;
-        } else {
-            const originalColor = window.getComputedStyle(el).color;
-            el.style.color = adjustBrightness(originalColor, brightness);
-        }
-    });
-});
-
-function adjustBrightness(color, brightness) {
-    const rgb = color.match(/\d+/g);
-    if (!rgb) return color;
-
-    let [r, g, b] = rgb.map(Number);
-    const maxBrightness = Math.max(r, g, b);
-
-    if (maxBrightness > 0) {
-        const multiplier = brightness / 100 * (255 / maxBrightness);
-        r = Math.min(255, r * multiplier);
-        g = Math.min(255, g * multiplier);
-        b = Math.min(255, b * multiplier);
-    }
-
-    return `rgb(${r}, ${g}, ${b})`;
-}
-
-
-
 document.querySelector('.email-icon').addEventListener('click', function() {
     // 이메일 텍스트를 클립보드에 복사
     const email = this.innerText;
@@ -67,3 +23,7 @@ document.querySelector('.email-icon').addEventListener('click', function() {
         copiedMessage.style.display = 'none';
     }, 2000);
 });
+
+
+
+
